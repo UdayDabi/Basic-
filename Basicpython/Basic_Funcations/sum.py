b140732df287937969320840a6bcd72ddd7436e8
Ayushdabi <udaydabi7@gmail.com>
2026-07-08 18:58:57 +0530

    done

--- Basicpython/Basic_Funcations/sum.py
+++ Basicpython/Basic_Funcations/sum.py
@@ -1,27 +1,10 @@
-def sum():
-    a = int(input("Enter first number: "))
-    b = int(input("Enter second number: "))
+def sum(a,b):
+     c= a+b
+     print("The sum of", a, "and", b, "is", c)
+     # return c
+
+# print(sum(5, 10))
+def sum_daf(a, b=0):
     c = a + b
-    print("The sum of", a, "and", b, "is:", c)
-
-def sum1(a, b):
-    c = a + b
     return c
-
-def divide(a, b):
-    c = a / b
-    return c
-
-def multiply(a, b):
-    c = a * b
-    return c
-def subtract(a, b):
-    c = a - b
-    return print("The sum of", a, "and", b, "is:" ,c)
-
-# print(sum1(10,30))
-# print(divide(10,2))
-# print(multiply(10,2))
-print(subtract(10,2))
-
-# sum()
+print(sum_daf(5, ))
