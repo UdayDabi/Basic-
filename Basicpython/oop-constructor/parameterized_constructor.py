2a0635d18218fe1d71ae2cb254ad704c9343eb08
Ayushdabi <udaydabi7@gmail.com>
2026-07-13 13:25:55 +0530

    done

--- Basicpython/oop-constructor/parameterized_constructor.py
+++ Basicpython/oop-constructor/parameterized_constructor.py
@@ -1,27 +1,28 @@
 class Shape:
-    def __init__(self, color, borderWidth):
-        self.color = color
-        self.borderWidth = borderWidth
+    def __init__(self):
+        self.color = ''
+        self.borderWidth = 0.0
 
-    # def setColor(self, c):
-    #     self.color = c
+    def setColor(self, c):
+        self.color = c
 
     def getColor(self):
         return self.color
 
-    # def setBorderWidth(self, borderWidth):
-    #     self.borderWidth = borderWidth
+    def setBorderWidth(self, borderWidth):
+        self.borderWidth = borderWidth
 
     def getBorderWidth(self):
         return self.borderWidth
 
 
-s = Shape("Red", 5)
-print("Color", s.getColor())
-print("BorderWidth", s.getBorderWidth())
+# s = Shape("Red", 5)
+# print("Color", s.getColor())
+# print("BorderWidth", s.getBorderWidth())
 
- # by Set Get Method 
-# s.setColor("Blue")
-# s.setBorderWidth(10)
-# print("Color", s.getColor())         # Blue
-# print("BorderWidth", s.getBorderWidth())   # 10
+ # by Set Get Method
+s = Shape()
+s.setColor("Blue")
+s.setBorderWidth(10)
+print("Color", s.getColor())         # Blue
+print("BorderWidth", s.getBorderWidth())   # 10
